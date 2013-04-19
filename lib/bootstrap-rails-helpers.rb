1a55cf395ed240875716297067c97e530ebed9de
Jacob Carlborg <jacob.carlborg@tretti.se>
2013-04-19 13:38:25 +0200

    Some cleanup.

--- lib/bootstrap-rails-helpers.rb
+++ lib/bootstrap-rails-helpers.rb
@@ -2,17 +2,17 @@ require "bootstrap-rails-helpers/core_ext/abstract"
 
 module BootstrapRailsHelpers
   module Renderers
-    autoload :Renderer, "bootstrap-rails-helpers/renderers/renderer"
     autoload :AbstractButtonRenderer, "bootstrap-rails-helpers/renderers/abstract_button_renderer"
     autoload :ButtonRenderer, "bootstrap-rails-helpers/renderers/button_renderer"
+    autoload :Renderer, "bootstrap-rails-helpers/renderers/renderer"
   end
 
   module Helpers
-    autoload :OptionsHelper, "bootstrap-rails-helpers/helpers/options_helper"
-    autoload :BaseHelper, "bootstrap-rails-helpers/helpers/base_helper"
     autoload :AlertHelper, "bootstrap-rails-helpers/helpers/alert_helper"
+    autoload :BaseHelper, "bootstrap-rails-helpers/helpers/base_helper"
     autoload :ButtonHelper, "bootstrap-rails-helpers/helpers/button_helper"
     autoload :FormTagHelper, "bootstrap-rails-helpers/helpers/form_tag_helper"
     autoload :LabelHelper, "bootstrap-rails-helpers/helpers/label_helper"
+    autoload :OptionsHelper, "bootstrap-rails-helpers/helpers/options_helper"
   end
 end
